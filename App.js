16db371026d4f0cccb0bf32467da355aed846dfe
Lucky Singh <singhlucky007007@gmail.com>
2023-05-15 20:53:16 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const port = process.env.PORT || 4000;
+const app = express();
+const router = require("./Routes/route")
+
+app.use("/",router)
+
+
+
+
+app.listen(port,()=>{console.log(` server is running on this port ${port}`)})
